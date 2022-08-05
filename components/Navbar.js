8a0592dd60e10f7440c2dc2ce31d6d499c4975b7
Sean Chester <sean.chester02@gmail.com>
2022-08-04 17:41:46 -0700

    spacing for navbar mid progress

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,32 @@
 import styles from '../styles/Navbar.module.css'
 import Link from 'next/link'
+import {FaSearch} from 'react-icons/fa'
 
 const Navbar = () => {
   return (
     <div className={styles.navHeader}>
-        <Link href="/"><a>Home</a></Link>
-        <Link href="/about"><a>About</a></Link>
+      <div className={styles.contentContainer}>
+        <div className={styles.contentLinks}>
+          <ul className={styles.ulForLinks}>
+           <li>
+            <Link href="/mens"><a>Men</a></Link>
+           </li> 
+           <li>
+            <Link href="/womens"><a>Women</a></Link>
+           </li>
+           <li>
+            <Link href="/kids"><a>Kids</a></Link>
+           </li>
+          </ul>
+        </div>
+        <div className={styles.homeTitleLink}>
+         <Link href="/"><a>Home</a></Link>
+        </div>
+        <div className={styles.headerRightIcons}>
+           <FaSearch className={styles.faSearchIcon}/>
+           <Link href="/about"><a>About</a></Link>
+        </div>
+      </div>
     </div>
   )
 }
