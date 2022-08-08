548873617888f09dbdf59fdd65b94aafc49e5e1f
Sean Chester <sean.chester02@gmail.com>
2022-08-08 16:23:07 -0700

    separated clothing categories from rest of navbar and centered searchbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,30 @@
-import { useState } from 'react'
 import styles from '../styles/Navbar.module.css'
 import Link from 'next/link'
 import {FaSearch, FaShoppingCart,} from 'react-icons/fa'
 import {CgProfile} from 'react-icons/cg'
 
 const Navbar = () => {
-  const [searchBar, setSearchBar] = useState(false);
 
-  const renderSearchBar = () => {
-    setSearchBar(true)
-  }
   return (
-    <div className={styles.navHeader}>
+    <div className={styles.navContainer}>
+     <div className={styles.navHeader}>
       <div className={styles.contentContainer}>
-        <div className={styles.contentLinks}>
+        <div className={styles.homeTitleLink}>
+         <Link href="/"><a>Ephesus Clothing</a></Link>
+        </div>
+        <div className={styles.searchBarContainer}>
+          <input type="text" /> <button>Search</button>
+        </div>
+        <div className={styles.headerRightIcons}>
+          <ul>
+            <li><CgProfile /></li>
+            <li><Link href="/shoppingCart"><span><div className={styles.shoppingCartCount}>2</div><FaShoppingCart /></span></Link></li>
+            <li><Link href="/about"><a>About</a></Link></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div className={styles.contentLinks}>
           <ul className={styles.ulForLinks}>
            <li>
             <Link href="/womens"><a>Women</a></Link>
@@ -29,22 +40,6 @@ const Navbar = () => {
            </li>
           </ul>
         </div>
-        <div className={styles.homeTitleLink}>
-         <Link href="/"><a>Ephesus Clothing</a></Link>
-        </div>
-        <div className={styles.headerRightIcons}>
-          <ul>
-            {searchBar && (<div>
-              <input type="text" /> <button>Search</button>
-            </div>
-            )}
-            {searchBar ? <div></div> : <li onClick={renderSearchBar}><FaSearch className={styles.faSearchIcon}/></li> }
-            <li><CgProfile /></li>
-            <li><div className={styles.shoppingCartCount}>2</div><FaShoppingCart /></li>
-            <li><Link href="/about"><a>About</a></Link></li>
-          </ul>
-        </div>
-      </div>
     </div>
   )
 }
